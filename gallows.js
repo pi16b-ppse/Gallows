485b7bf99a352849a0bb50c79b66ad37882f8536
Olga Fesenko <Olga.Fesenko29@gmail.com>
2019-04-09 15:59:15 +0300

    Добавлено исключение повторения отгаданных слов

--- gallows.js
+++ gallows.js
@@ -1,18 +1,44 @@
-var array = ["ПРОГРАММИСТ", "ПРОГРАММА", "ИНКАПСУЛЯЦИЯ", "НАСЛЕДОВАНИЕ", "ПОЛИМОРФИЗМ", "ПАТТЕРН", "РЕКУРСИЯ", "КОД",
-"КЛАСС", "ПЕРЕМЕННАЯ", "МАССИВ", "БИТ", "БАЙТ", "БАЗА ДАННЫХ", "ЗАПРОС", "МАКРОС",
-"ФОРМА", "ФУНКЦИЯ", "ДАННЫЕ", "КОНСТАНТА", "ГРАФ", "ДЕРЕВО", "МАССИВ", "МАТРИЦА",
-"КОМПИЛЯТОР"];
+let array = ["ПРОГРАММИСТ", "ПРОГРАММА", "ИНКАПСУЛЯЦИЯ", "НАСЛЕДОВАНИЕ", "ПОЛИМОРФИЗМ", "ПАТТЕРН", "РЕКУРСИЯ", "КОД",
+    "КЛАСС", "ПЕРЕМЕННАЯ", "МАССИВ", "БИТ", "БАЙТ", "БАЗА ДАННЫХ", "ЗАПРОС", "МАКРОС",
+    "ФОРМА", "ФУНКЦИЯ", "ДАННЫЕ", "КОНСТАНТА", "ГРАФ", "ДЕРЕВО", "МАССИВ", "МАТРИЦА",
+    "КОМПИЛЯТОР"
+];
 
-var random = Math.floor(Math.random() * array.length);
+let aL;
 
-var word0 = array[random];
+const ususus = array.length;
 
-var length = word0.length;
+let random, word0, length, word1;
 
-var word1 = "";
+if (sessionStorage.getItem('aL') == null) {
+    aL = ususus;
+    random = Math.floor(Math.random() * aL);
+    word0 = array[random];
+    length = word0.length;
+    word1 = "";
+}
 
-for(i = 0; i < length; i++) {
-    if(word0.charAt(i) == " ") {
+if (sessionStorage.getItem('aL') > 0) {
+    aL = sessionStorage.getItem('aL');
+    for (i = 0; i < aL; i++) {
+        array[i] = sessionStorage['arr[' + i + ']'];
+    }
+    random = Math.floor(Math.random() * aL);
+    word0 = array[random];
+    length = word0.length;
+    word1 = "";
+}
+
+if (sessionStorage.getItem('aL') == 0) {
+    aL = ususus;
+    random = Math.floor(Math.random() * aL);
+    word0 = array[random];
+    length = word0.length;
+    word1 = "";
+}
+
+for (i = 0; i < length; i++) {
+    if (word0.charAt(i) == " ") {
         word1 = word1 + " ";
     } else {
         word1 = word1 + "-";
@@ -24,7 +50,8 @@ const soundYes = new Audio("sound/sound_yes.wav");
 const soundNo = new Audio("sound/sound_no.wav");
 
 var letters = ["А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О",
-"П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"];
+    "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"
+];
 
 let count = 0;
 
@@ -34,11 +61,13 @@ let correctCount;
 
 if (sessionStorage.getItem('correctCount') == null) {
     correctCount = 0;
-    alert(correctCount);
 }
 if (sessionStorage.getItem('correctCount') > 0) {
     correctCount = sessionStorage.getItem('correctCount');
-    alert(correctCount);
+}
+
+if (sessionStorage.getItem('correctCount') == ususus) {
+    correctCount = 0;
 }
 
 // Вывод слова по буквам
@@ -51,10 +80,10 @@ window.onload = start;
 // Вывод букв
 function start() {
     var content = "";
-    for(i = 0; i <= 32; i++) {
+    for (i = 0; i <= 32; i++) {
         var element = "let" + i;
-        content = content + '<div class="letter" onclick="check('+ i +')" id="'+ element +'">' + letters[i] + '</div>';
-        if((i + 1) % 6 == 0) {
+        content = content + '<div class="letter" onclick="check(' + i + ')" id="' + element + '">' + letters[i] + '</div>';
+        if ((i + 1) % 6 == 0) {
             content = content + '<div style="clear:both;"></div>';
         }
     }
@@ -62,23 +91,24 @@ function start() {
     printLetter();
 };
 
-String.prototype.contains = function(a, b) {
-    if(a > this.length - 1) {
+String.prototype.contains = function (a, b) {
+    if (a > this.length - 1) {
         return this.toString();
     } else {
         return this.substr(0, a) + b + this.substr(a + 1);
     }
 }
+
 // Проверка на правильность введённых букв загаданного слова и вывод результатов
 function check(y) {
     var correct = false;
-    for(i = 0; i < length; i++) {
-        if(word0.charAt(i) == letters[y]) {
+    for (i = 0; i < length; i++) {
+        if (word0.charAt(i) == letters[y]) {
             word1 = word1.contains(i, letters[y]);
             correct = true;
         }
     }
-    if(correct == true) {
+    if (correct == true) {
         soundYes.play();
         var element = "let" + y;
         document.getElementById(element).style.background = "#000000";
@@ -86,25 +116,25 @@ function check(y) {
         document.getElementById(element).style.border = "5px solid #00FF00";
         document.getElementById(element).style.cursor = "default";
         printLetter();
-        if(count == 0) {
+        if (count == 0) {
             var img = "picture_after/picture_0.png";
-            document.getElementById("gallows").innerHTML = '<img src="'+ img +'" height="450" width="400" />';
+            document.getElementById("gallows").innerHTML = '<img src="' + img + '" height="450" width="400" />';
         }
-        if(count == 1) {
+        if (count == 1) {
             var img = "picture_after/picture_1.png";
-            document.getElementById("gallows").innerHTML = '<img src="'+ img +'" height="450" width="400" />';
+            document.getElementById("gallows").innerHTML = '<img src="' + img + '" height="450" width="400" />';
         }
-        if(count == 2) {
+        if (count == 2) {
             var img = "picture_after/picture_2.png";
-            document.getElementById("gallows").innerHTML = '<img src="'+ img +'" height="450" width="400" />';
+            document.getElementById("gallows").innerHTML = '<img src="' + img + '" height="450" width="400" />';
         }
-        if(count == 3) {
+        if (count == 3) {
             var img = "picture_after/picture_3.png";
-            document.getElementById("gallows").innerHTML = '<img src="'+ img +'" height="450" width="400" />';
+            document.getElementById("gallows").innerHTML = '<img src="' + img + '" height="450" width="400" />';
         }
-        if(count == 5) {
+        if (count == 5) {
             var img = "picture_after/picture_4.png";
-            document.getElementById("gallows").innerHTML = '<img src="'+ img +'" height="450" width="400" />';
+            document.getElementById("gallows").innerHTML = '<img src="' + img + '" height="450" width="400" />';
         }
     } else {
         soundNo.play();
@@ -115,18 +145,34 @@ function check(y) {
         document.getElementById(element).style.cursor = "default";
         document.getElementById(element).setAttribute("onclick", ";");
         count++;
-        var img = "picture_befor/picture_"+ count + ".png";
-        document.getElementById("gallows").innerHTML = '<img src="'+ img +'" height="450" width="400" />';
+        var img = "picture_befor/picture_" + count + ".png";
+        document.getElementById("gallows").innerHTML = '<img src="' + img + '" height="450" width="400" />';
     }
-    if(word0 == word1) {
+    if (word0 == word1) {
         correctCount++;
         sessionStorage.setItem('correctCount', correctCount);
-        alert(correctCount);
-        document.getElementById("alphabet").innerHTML = "ВЫ ПОБЕДИЛИ!" + '<p>ЗАГАДАННОЕ СЛОВО: ' + word0 + '</p>'
-        + '<p><span class="reset" onclick="location.reload();">СЫГРАЕТЕ ЕЩЁ РАЗ?</span></p>';
+
+        aL--;
+        sessionStorage.setItem('aL', aL);
+
+        array.splice(array.indexOf(word0), 1);
+        for (i = 0; i < aL; i++) {
+            sessionStorage['arr[' + i + ']'] = array[i];
+        }
+
+        if (correctCount == ususus) {
+            document.getElementById("alphabet").innerHTML = "ВЫ ОТГАДАЛИ ВСЕ СЛОВА!" + '<p>ЗАГАДАННОЕ СЛОВО: ' + word0 + '</p>' +
+                '<p>ОТГАДАНО СЛОВ: ' + correctCount + ' ВСЕГО СЛОВ: ' + ususus + '</p>' +
+                '<p><a href="start.html" class="reset">ВЕРНУТСЯ В ГЛАВНОЕ МЕНЮ!</a></p>';
+        } else {
+            document.getElementById("alphabet").innerHTML = "ВЫ ПОБЕДИЛИ!" + '<p>ЗАГАДАННОЕ СЛОВО: ' + word0 + '</p>' +
+                '<p>ОТГАДАНО СЛОВ: ' + correctCount + ' ВСЕГО СЛОВ: ' + ususus + '</p>' +
+                '<p><span class="reset" onclick="location.reload();">СЫГРАЕТЕ ЕЩЁ РАЗ?</span></p>';
+        }
     }
-    if(count >= 6) {
-        document.getElementById("alphabet").innerHTML = "ВЫ ПРОИГРАЛИ!" + '<p>ЗАГАДАННОЕ СЛОВО: ' + word0 + '</p>'
-        + '<p><span class="reset" onclick="location.reload();">СЫГРАЕТЕ ЕЩЁ РАЗ?</span></p>';
+    if (count >= 6) {
+        document.getElementById("alphabet").innerHTML = "ВЫ ПРОИГРАЛИ!" + '<p>ЗАГАДАННОЕ СЛОВО: ' + word0 + '</p>' +
+            '<p>ОТГАДАНО СЛОВ: ' + correctCount + ' ВСЕГО СЛОВ: ' + ususus + '</p>' +
+            '<p><span class="reset" onclick="location.reload();">СЫГРАЕТЕ ЕЩЁ РАЗ?</span></p>';
     }
 };
